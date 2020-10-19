b89c84497c7c18b56b1b04a275912630579a50b8
Diego Dom <dominguezdiex@gmail.com>
2020-10-18 21:59:54 -0500

    Login y autenticación

--- resources/views/recepies/index.blade.php
+++ resources/views/recepies/index.blade.php
@@ -1,13 +1,23 @@
-<h1>Recetas</h1>
-@foreach ($recepies as $recepie)
-    <ul>
-        <li>{{ $recepie }}</li>
-    </ul>
-@endforeach
-<br>
-<h2>Categorias</h2>
-@foreach ($categories as $category)
-    <ul>
-        <li>{{ $category }}</li>
-    </ul>
-@endforeach
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Recetas</h1>
+                @foreach ($recepies as $recepie)
+                    <ul>
+                        <li>{{ $recepie }}</li>
+                    </ul>
+                @endforeach
+                <br>
+                <h2>Categorias</h2>
+                @foreach ($categories as $category)
+                    <ul>
+                        <li>{{ $category }}</li>
+                    </ul>
+                @endforeach
+            </div>
+        </div>
+    </div>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/recetas', RecepiesController::class);
+
+Auth::routes();
+
+/* Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home'); */
